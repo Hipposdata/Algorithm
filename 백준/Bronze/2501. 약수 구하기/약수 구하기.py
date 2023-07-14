ec8436f04c3b93b6cdfc41804290dd71e0648d32
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2023-07-14 11:04:50 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+import sys
+N, K = map(int,sys.stdin.readline().split())
+
+lst = []
+
+for i in range(1,N+1):
+    if N % i == 0:
+        lst.append(i)
+if len(lst) <K :
+    print(0)
+else:
+    print(lst[K-1])
