a360713f9f0bb74e054e6788e99bd44c9f795e65
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-06-23 11:11:54 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,9 @@
+def solution(seoul):
+
+    for i in range(len(seoul)):
+        if seoul[i] == 'Kim':
+            dap  = i
+            
+    answer = f'김서방은 {dap}에 있다'
+        
+    return answer
