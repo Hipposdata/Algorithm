8721610e3e5db0b8133ff64c5ed35cf1028dc938
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-09-09 16:12:47 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,4 +1,11 @@
-N = input()
-N= list(N)
-N.sort(reverse=True)
-print("".join(N))
+#선택정렬
+import sys
+
+n = list(map(int,sys.stdin.readline().strip()))
+
+for idx in range(len(n)):
+    mx_idx = n[idx:].index(max(n[idx:])) + idx
+    if n[mx_idx] > n[idx]:
+        n[mx_idx], n[idx] = n[idx], n[mx_idx]
+
+print(''.join(map(str,n)))
