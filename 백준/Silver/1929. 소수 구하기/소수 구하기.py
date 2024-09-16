309ea4ab36276f48ce1e0123ac4feee14a3e9665
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-09-17 00:29:35 +0900

    [Silver III] Title: 소수 구하기, Time: 512 ms, Memory: 46468 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,12 +1,15 @@
+# 정수론 소수구하기
 import sys
+m, n = map(int,sys.stdin.readline().split())
 
-#  약수가 존재하면 소수가 아님을 이용
-m,n = map(int,sys.stdin.readline().split())
-for i in range(m,n+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5)+1):
-        if i %j ==0:
-            break
-    else:
-        print(i)
+
+
+lst = [0] *2 + [1] * (n-1)
+
+for j in range(2, int(n**(1/2))+1 ):
+    for k in range(j*j,n+1,j):
+        lst[k] = 0
+
+for l in range(2, n+1):
+    if l >= m and lst[l] == 1:
+        print(l)
