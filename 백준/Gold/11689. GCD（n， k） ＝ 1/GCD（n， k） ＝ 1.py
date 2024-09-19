042c1ba298ec2c1133b786c77871d606d820662e
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-09-19 22:43:14 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 144 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,16 @@
+
+import sys
+n = int(sys.stdin.readline())
+sosu_cont = n
+
+# 입력값의 소수개수만 알면 되므로 각각 계산해줌
+for j in range(2, int(n**0.5)+1):
+    if n % j ==0: # 소인수 확인
+        sosu_cont = sosu_cont - int(sosu_cont/j)
+        while n % j == 0: # 다음 소인수 n 찾기
+            n = n // j
+
+# 루트까지 확인하고 남은 소수1개 n 확인
+if n >1:
+    sosu_cont = sosu_cont - int(sosu_cont / n)
+print(sosu_cont)
