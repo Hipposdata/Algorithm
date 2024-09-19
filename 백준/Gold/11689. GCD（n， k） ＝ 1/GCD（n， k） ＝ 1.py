e3a5c50397e00ae9e74e6e6826ee461c4444fab7
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-09-19 22:49:58 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -10,7 +10,8 @@ for j in range(2, int(n**0.5)+1):
         while n % j == 0: # 다음 소인수 n 찾기
             n = n // j
 
-# 루트까지 확인하고 남은 소수1개 n 확인
+# 루트까지 확인하고 남은 소인수1개 n 확인(루트까지 반복해도 없다면
+# 최초입력받은 n자체가 소인수임
 if n >1:
     sosu_cont = sosu_cont - int(sosu_cont / n)
 print(sosu_cont)
