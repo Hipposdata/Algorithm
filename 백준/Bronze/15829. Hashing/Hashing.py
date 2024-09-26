50bbdd2862fc7da76701ccec124806760b48824e
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-09-26 09:46:52 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,10 @@
 import sys
 alpha = {chr(i): i - 96 for i in range(97, 123)}
 
-n = int(sys.stdin.readline())
-ch = sys.stdin.readline()
+n = int(sys.stdin.readline().strip())
+ch = sys.stdin.readline().strip()
 
 dap = 0
 for i in range(n):
     dap += alpha[ch[i]] *(31**i)
-print(dap)
+print(dap % 1234567891)
