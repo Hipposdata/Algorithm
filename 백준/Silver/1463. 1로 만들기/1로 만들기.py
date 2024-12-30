32de01d7208cb7a125c6ded517de4245cf584f5d
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-12-30 20:57:40 +0900

    [Silver III] Title: 1로 만들기, Time: 464 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,16 @@
 import sys
+
 n = int(sys.stdin.readline())
 
-dp = [0] * (n+1)
-# 각 인덱스는의 값은 해당 숫자 연산의 최소 수
+dp = [0]*(n+1)
+
+dp[1] = 0
 
 for i in range(2,n+1):
-
-    # 1빼는 연산
-    dp[i] = dp[i-1]+1
-    # 2로 나눠지는 연산
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] +1)
-    # 3으로 나눠지는 연산
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+    dp[i] = dp[i-1] +1
+    if i %2 ==0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i%3 ==0:
+        dp[i] = min(dp[i],dp[i//3]+1)
 
 print(dp[n])
