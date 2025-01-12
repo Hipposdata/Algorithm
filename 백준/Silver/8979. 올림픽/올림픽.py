08025ae4012d37d5d566d66f383be7fd1438d547
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2025-01-12 23:52:56 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,19 +9,21 @@ for _ in range(n):
 
 lst.sort(key=lambda x: (x[1],x[2],x[3]),reverse = True)
 
-rank =1
+target = 0
 # 등수 세기
 for i in range(0,len(lst)):
     if lst[i][0] == k:
+        target += 1
         break
     else:
-        rank+=1
+        target+=1
 
 # 같은거 개수 찾기
-same = 0
+rank = 0
 for j in range(0,len(lst)):
-    if lst[rank-1] == lst[j]:
-        same +=1
-
-print(rank - same)
+    if lst[target-1][1:] == lst[j][1:]:
+        print(rank +1)
+        break
+    else:
+        rank +=1
 
