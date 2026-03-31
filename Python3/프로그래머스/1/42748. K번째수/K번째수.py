875285c9fdf5f29563889164a1252039e7dc4582
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2026-04-01 00:19:47 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i,j,k in commands:
+        lst = array[i-1:j]
+        lst.sort()
+        answer.append(lst[k-1])
+        
+    return answer
