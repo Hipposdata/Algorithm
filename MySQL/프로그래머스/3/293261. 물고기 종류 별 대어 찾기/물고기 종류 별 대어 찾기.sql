6c51f52b16b5e4e03e932efcb080beceeba3a4fa
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2026-04-01 17:17:17 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,15 @@
+-- 코드를 작성해주세요
+SELECT I.ID, N.FISH_NAME, I.LENGTH
+FROM FISH_INFO AS I
+JOIN FISH_NAME_INFO AS N
+ON I.FISH_TYPE = N.FISH_TYPE
+WHERE (I.FISH_TYPE, I.LENGTH) IN(
+    SELECT FISH_TYPE, MAX(LENGTH) 
+    # 각 물고기 종류별 종류,길이 추출
+    FROM FISH_INFO
+    GROUP BY FISH_TYPE)
+ORDER BY I.ID ASC;
+
+
+
+# 물고기 종류별 길이 MAX 찾기 -> 동일한 아이디의 추가정보 표출 
