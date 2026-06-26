dd38723ae20dc1baa8236abbb010f418ad7a79e9
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2026-06-27 00:59:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.79 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 9.39 MB, 시간: 13.28 ms
+메모리: 11.2 MB, 시간: 7.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 25일 16:06:10
+2026년 06월 27일 00:59:54
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,55 +1,42 @@
-# 최솟값 -> bfs 
-
 from collections import deque
 
 def solution(maps):
     answer = 0
-    # 방향 하좌상우
+    row_len = len(maps)
+    col_len = len(maps[0])
+    
+    # 방향정의 하좌상우
     dr = [1,0,-1,0]
-    dc = [0,-1,0,1]
+    dc = [0, -1,0,1]
     
+    # 최단거리 -> bfs
     q = deque()
-    q.append((0,0))
-    
-    # 행, 열 길이
-    n = len(maps)
-    m = len(maps[0])
-    
-    # 방문기록
-    vst = [[False] * m for _ in range(n)]
+    q.append((0,0,1))
+
+    # 방문기록 
+    vst = [ [False] * col_len for _ in range(row_len)]
     vst[0][0] = True
     
-    # 해당위치 이동 수
-    step = [[1] * m for _ in range(n)]
-    
-    # 행, 열 길이
-    n = len(maps)
-    m = len(maps[0])
-    
+    # 큐에 좌표, 거리 넣음
     while q:
-        cur_r, cur_c = q.popleft()
+        cur_r, cur_c, dist = q.popleft()
         
-        if vst[n-1][m-1] == True:
+        if cur_r == row_len-1 and cur_c == col_len -1:
             break
         
         for i in range(4):
             nr = cur_r + dr[i]
             nc = cur_c + dc[i]
             # 범위 내
-            if 0<=nr<= n-1 and 0<=nc<=m-1:
-                # 방문X 
-                if vst[nr][nc] == False:
-                    # 벽 없음
-                    if maps[nr][nc] == 1:
-                        step[nr][nc] = step[cur_r][cur_c] +1
-                        vst[nr][nc] = True
-                        q.append((nr,nc))
-    
-    
-    if vst[n-1][m-1] == True:
-        answer = step[n-1][m-1]
-    else:
+            if 0<=nr <=row_len-1 and 0<=nc<=col_len-1:
+                
+                # 장애물X, 방문X 
+                if vst[nr][nc] ==False and maps[nr][nc] ==1:
+                    vst[nr][nc] = True
+                    q.append((nr, nc, dist +1))            
+                    
+    if vst[-1][-1] == False:
         answer = -1
-    
-    
+    else:
+        answer = dist
     return answer
