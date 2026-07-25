b322cf2c2f72b9f8d6053cc65b2848b196e6748b
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2026-07-26 08:40:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.07 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,52 +1,49 @@
-# 빠르게 도달 -> BFS
-
-from collections import deque 
+# 최단거리 bfs
+from collections import deque
 
 def solution(maps):
     answer = 0
     
+    # 행렬
     col = len(maps[0])
     row = len(maps)
     
-    # 방문기록 
-    vst = [[False] * col for _ in range(row)]
-    vst[0][0] = True
-    
-    # 방향지정 하좌상우
+    # 방향 하좌상우
     dr = [1,0,-1,0]
     dc = [0,-1,0,1]
     
-    # 위치 이동수  / 첫칸은 1 
-    step = [[1] * col for _ in range(row)]
+    # 방문여부
+    vst = [[False] * (col) for _ in range(row)]
+    vst[0][0] = True
     
+    # 이동기록
+    step = [[-1] * (col) for _ in range(row)]
+    step[0][0] = 1
+
     q = deque()
     q.append((0,0))
     
     while q:
         cur_r, cur_c = q.popleft()
         
-        # 마지막 도착지 방문 -> 멈춤
-        if vst[row-1][col-1] == True:
-            break
         
-        # 다음 이동위치 선정 
         for i in range(4):
-            nr = cur_r + dr[i]
-            nc = cur_c + dc[i]
+            nxt_r = cur_r + dr[i]
+            nxt_c = cur_c + dc[i]
             
-            # 범위내
-            if 0<=nr<= row-1 and 0<= nc <=col-1:
-                # 방문X
-                if vst[nr][nc] == False:
-                    # 벽 없음
-                    if maps[nr][nc] == 1:
-                        step[nr][nc] = step[cur_r][cur_c] +1
-                        vst[nr][nc] = True
-                        q.append((nr, nc))
-        if vst[row-1][col-1] == False:
-            answer = -1
-        else:
-            answer = step[row-1][col-1]
-    
+            # 범위 내 
+            if 0<= nxt_r < row and 0<= nxt_c < col:
+                # 벽없음, 방문 X 
+                if maps[nxt_r][nxt_c] == 1 and vst[nxt_r][nxt_c] == False:
+                    vst[nxt_r][nxt_c] = True
+                    step[nxt_r][nxt_c] = step[cur_r][cur_c] +1
+                    q.append((nxt_r, nxt_c))
+                    
+                    
+    if step[-1][-1] == -1:
+        answer = -1
+    else:
+        answer = step[-1][-1]
+                    
     
     return answer
