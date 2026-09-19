6c252ae37077a423a53f9f820f4e575eb1612112
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2026-09-19 15:51:19 +0900

    [level 3] Title: 입국심사, Time: 240.63 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ Python/프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,24 @@
+def solution(n, times):
+    answer = 0
+    time = 0
+    
+    left = 1
+    right =  min(times) * n
+    
+    while left<= right:
+        time = (left + right) //2 
+        
+        cnt = 0
+        for i in times:
+            cnt += time // i
+            
+            
+        if cnt < n:
+            left = time +1
+        else:
+            right = time -1
+
+    
+    answer = left 
+    
+    return answer
